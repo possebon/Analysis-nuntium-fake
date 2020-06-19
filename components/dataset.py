188ddca5a6294ff1fe0f6de89745e5a7632284a2
Felipe Boff Nunes <felipeboffnunes@protonmail.com>
2020-06-19 09:53:47 -0300

    data

--- components/dataset.py
+++ components/dataset.py
@@ -1,8 +1,11 @@
 # Python Standard Libraries
+import time
 from pathlib import Path
+from multiprocessing import Pool
 # External Libraries
 import pandas as pd
-
+# Components
+from nlp import get_polarity_and_subjectivity_from_string, get_polarity_scores, translate_to_en
 
 def load_data_as_dataframe() -> list:
     """Loads data from Dataset as Dataframe
@@ -45,6 +48,8 @@ def load_data_as_dataframe() -> list:
             else:
                 id = int(file.stem)
                 
+            print(id)
+                
             # Separate data for categories
             if veracity == "true":
                 df_true.loc[id] = [veracity, data]
@@ -65,4 +70,50 @@ def load_data_as_dataframe() -> list:
     df_fake_meta.sort_index()        
             
     return [df_true, df_fake, df_true_meta, df_fake_meta]
+ 
     
+def get_sentiment_dataframe(df_true, df_fake) -> list:
+    # Initialize Dataframes
+    df_true_sentiment = pd.DataFrame(columns=["TextBlob Polarity", "TextBlob Subjectivity",\
+        "VADER negative", "VADER neutral", "VADER positive", "VADER compound"])
+    df_fake_sentiment = pd.DataFrame(columns=["TextBlob Polarity", "TextBlob Subjectivity",\
+        "VADER negative", "VADER neutral", "VADER positive", "VADER compound"])
+
+    
+    # Get nlp data
+    for id, row in df_true.iterrows():
+        print(id)
+        translated_news = translate_to_en(row["News"])
+        print(translated_news)
+        polarity, subjectivity = get_polarity_and_subjectivity_from_string(translated_news)
+        vader = get_polarity_scores(translated_news)
+        df_true_sentiment.loc[id] = [polarity, subjectivity, vader["neg"], vader["neu"], vader["pos"], vader["compound"]]
+        
+
+    for id, row in df_fake.iterrows():
+        print(id)
+        translated_news = translate_to_en(row["News"])
+        print(translated_news)
+        polarity, subjectivity = get_polarity_and_subjectivity_from_string(translated_news)
+        vader = get_polarity_scores(translated_news)
+        df_fake_sentiment.loc[id] = [polarity, subjectivity, vader["neg"], vader["neu"], vader["pos"], vader["compound"]]
+    
+    # Sort index for Dataframes    
+    df_true_sentiment.sort_index()
+    df_fake_sentiment.sort_index()
+        
+    return [df_true_sentiment, df_fake_sentiment]
+        
+dft, dff, dftm, dffm = load_data_as_dataframe()
+
+dfts, dffs = get_sentiment_dataframe(dft, dff)
+
+def download_dataframe_as_csv(df, name:str, path:str="../data_csv/") -> None:
+    df.to_csv(f"{path}/{name}.csv", index=True)
+    
+download_dataframe_as_csv(dft, "true")
+download_dataframe_as_csv(dff, "fake")
+download_dataframe_as_csv(dftm, "true-meta")
+download_dataframe_as_csv(dffm, "fake-meta")
+download_dataframe_as_csv(dfts, "true-sentiment")
+download_dataframe_as_csv(dffs, "fake-sentiment")
--- components/nlp.py
+++ components/nlp.py
@@ -1,7 +1,9 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
+from translate import Translator
 from textblob import TextBlob
+from faker import Faker
 import spacy
 
 
@@ -21,10 +23,6 @@ def remove_stop_word(tokens:list) -> list:
 
 def get_polarity_and_subjectivity_from_string(string:str) -> list:
     blob = TextBlob(string)
-    
-    if blob.detect_language() != "en":
-        blob = TextBlob(str(blob.translate(to="en")))
-        
     polarity = blob.sentiment.polarity
     subjectivity = blob.sentiment.subjectivity
     return [polarity, subjectivity]
@@ -40,16 +38,57 @@ def get_polarity_scores(string:str) -> dict:
     """    
     
     analyzer = SentimentIntensityAnalyzer()
-    
-    string = TextBlob(string)
-    if string.detect_language() != "en":
-        string = str(string.translate(to="en"))
         
     return analyzer.polarity_scores(string)
         
     
-    
-    
+def translate_to_en(string:str) -> str:
+    translator = get_new_translator()
+    divide = False
+    if len(string) > 500:
+        divide = True
+        first_index = 0
+        last_index = string[:500].rfind(".")
+    if not divide:    
+        translation = translator.translate(string)
+        
 
+    while divide:
+        translation = ""
+        translation_ = translator.translate(string[first_index:last_index])
+        
+        # Check if exceeded requests amount
+        while len(translation_) == 194 and "MYMEMORY" in translation_:
+            translator = get_new_translator()
+            translation_ = translator.translate(string[first_index:last_index])
+        translation += translation_
+        if len(string) - last_index > 500:
+            step = last_index + 500
+            print("Step: ", step)
+            range_index = string[last_index:step].rfind(".")
+            if range_index <= 0:
+                range_index = string[last_index:step].rfind(",")
+            if range_index <= 0:
+                range_index = string[last_index:step].rfind(" ")
+            print("Last index: ", last_index)
+            print("Range: ", range_index)
+            last_index = range_index + last_index
+            print("New Last index: ", last_index)
+            print("Len string: ", len(string))
+        else:
+            translation_ = translator.translate(string[last_index:])
+            # Check if exceeded requests amount
+            while len(translation_) == 194 and "MYMEMORY" in translation_:
+                translator = get_new_translator()
+                translation_ = translator.translate(string[first_index:last_index])
+            translation += translation_      
+            divide = False
+            
+    return translation
 
-
+def get_new_translator():
+    faker = Faker()
+    email = faker.email()
+    print(email)
+    translator = Translator(from_lang="pt-br", to_lang="en", email=email)
+    return translator
